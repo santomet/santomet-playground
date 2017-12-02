4e27695e24e70612fd83283e76e26c1cd00c8c29
Marek Šanta <teslan223@gmail.com>
2017-12-02 08:47:14 +0100

    added advent of code day 2

--- Advent2017/adventofcode.py
+++ Advent2017/adventofcode.py
@@ -1,7 +1,7 @@
 import math
 
-def advent1(input):
-    num_str_list = list(input)
+def advent1(data):
+    num_str_list = list(data)
     while num_str_list[0] == num_str_list[-1]:
         # fucking circle
         num_str_list.append(num_str_list[0])
@@ -34,8 +34,8 @@ def get_circ_index(index, count):
     return index
 
 
-def advent1_2(input):
-    num_str_list = list(input)
+def advent1_2(data):
+    num_str_list = list(data)
     count = len(num_str_list)
     halfway = round(count / 2)
     total = 0
@@ -44,3 +44,41 @@ def advent1_2(input):
         if num_str_list[i] == num_str_list[get_circ_index(i + halfway, count)]:
             total += int(num_str_list[i])
     return total
+
+
+def advent2(data):
+    row_list = data.split('\n')
+    total = 0
+    for row in row_list:
+        item_list = row.split()
+        row_total = 0
+        highest = 0
+        lowest = -1  # first number goes there
+        for item in item_list:
+            num = int(item)
+            if lowest == -1 or num < lowest:
+                lowest = num
+            if num > highest:
+                highest = num
+        total += (highest - lowest)
+    return total
+
+def get_first_even_divide(number, divider_list):  # func returns 0 if not evenly
+    for div in divider_list:
+        div = int(div)
+        if div == number: # easiest way
+            continue
+        if ((number % div) == 0):
+            return number // div
+    return 0
+
+def advent2_2(data):
+    row_list = data.split('\n')
+    total = 0
+    for row in row_list:
+        item_list = row.split()
+        for item in item_list: #yes, O(n^2), order matters!
+            num = int(item)
+            total += get_first_even_divide(num, item_list)
+    return total
+
