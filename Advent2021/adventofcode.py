72fc6ff33d90e755fa64a33adaeb1731fa007625
Marek Šanta <teslan223@gmail.com>
2021-12-03 20:26:19 +0100

    aaaa boring

--- Advent2021/adventofcode.py
+++ Advent2021/adventofcode.py
@@ -26,11 +26,97 @@ def advent1(data):
 
     print("First: {}, second {}".format(increasecount, increasegroupcount))
 
+def advent2(data):
+    lines = data.split('\n')
+
+    depth = 0
+    length = 0
+
+    aim = 0
+    length2 = 0
+    depth2 = 0
+
+    for l in lines:
+        cv = l.split(' ')
+        command = cv[0]
+        value = cv[1]
+        if command == "forward":
+            length += int(value)
+            length2 += int(value)
+            depth2 += int(value) * aim
+        elif command == "down":
+            depth += int(value)
+            aim += int(value)
+        elif command == "up":
+            depth -= int(value)
+            aim -= int(value)
+
+    print("First: {}, second {}".format(depth*length, depth2 * length2)) 
+
+def advent3_get_rests(lines, position, top):
+    totalsize = len(lines)
+    onecount = 0
+    topval = 0
+    
+    for l in lines:
+        if l[position] == "1":
+            onecount += 1
+
+    if onecount > totalsize / 2:
+        topval = 1
+
+    if onecount == totalsize / 2:
+        topval = 1
+
+    filt = []
+
+    
+    for l in lines:
+        if int(l[position]) == topval and top:
+            filt.append(l)
+        elif int(l[position]) != topval and not top:
+            filt.append(l)
 
 
+    return filt
+        
+
+def advent3(data):
+    lines = data.split('\n')
+    totalsize = len(lines)
+    bitwidth = len(lines[0])
+    onecounts = [0] * bitwidth
+
+    for l in lines:
+        for i in range(bitwidth):
+            if l[i] == "1":
+                onecounts[i] += 1
+
+    finals = ["0"] * bitwidth
+    finale = ["1"] * bitwidth
+    for i in range(bitwidth):
+        if onecounts[i] > totalsize/2:
+            finals[i] = "1"
+            finale[i] = "0"
+
+    sigma = int("".join(finals), base = 2)
+    epsilon = int("".join(finale), base = 2)
+
+    filtertop = lines
+    filterleast = lines
+    for i in range(bitwidth):
+        if(len(filtertop) > 1):
+            filtertop = advent3_get_rests(filtertop, i, True)
+        if(len(filterleast) > 1):
+            filterleast = advent3_get_rests(filterleast, i, False)
+
+    oxygen = int(filtertop[0], base = 2)
+    co2 = int(filterleast[0], base = 2)
+
+    print("First: {}, second {}".format(sigma*epsilon, oxygen*co2))
 
 def main():
-    last_solved = 1
+    last_solved = 3
     txt = """
    __,                            _  _  _  ,
   /  |   _|        _      _|_    / )/ \/ )/|
@@ -70,44 +156,44 @@ def main():
         
         if num == 1:
             advent1(data)
-##        elif num == 2:
-##            advent2(data)
-##        elif num == 3:
-##            advent3(data)
-##        elif num == 4:
-##            advent4(data)
-##        elif num == 5:
-##            advent5(data)
-##        elif num == 6:
-##            advent6(data)
-##        elif num == 7:
-##            advent7(data)
-##        elif num == 8:
-##            advent8(data)
-##        elif num == 9:
-##            advent9(data)
-##        elif num == 10:
-##            advent10(data)
-##        elif num == 11:
-##            advent11(data)
-##        elif num == 12:
-##            advent12(data)
-##        elif num == 13:
-##            advent13(data)
-##        elif num == 14:
-##            advent14(data)
-##        elif num == 15:
-##            advent15(data)
-##        elif num == 16:
-##            advent16(data)
-##        elif num == 17:
-##            advent17(data)
-##        elif num == 18:
-##            advent18(data)
-##        elif num == 19:
-##            advent19(data)
-##        elif num == 20:
-##            advent20(data)
+        elif num == 2:
+            advent2(data)
+        elif num == 3:
+            advent3(data)
+        elif num == 4:
+            advent4(data)
+        elif num == 5:
+            advent5(data)
+        elif num == 6:
+            advent6(data)
+        elif num == 7:
+            advent7(data)
+        elif num == 8:
+            advent8(data)
+        elif num == 9:
+            advent9(data)
+        elif num == 10:
+            advent10(data)
+        elif num == 11:
+            advent11(data)
+        elif num == 12:
+            advent12(data)
+        elif num == 13:
+            advent13(data)
+        elif num == 14:
+            advent14(data)
+        elif num == 15:
+            advent15(data)
+        elif num == 16:
+            advent16(data)
+        elif num == 17:
+            advent17(data)
+        elif num == 18:
+            advent18(data)
+        elif num == 19:
+            advent19(data)
+        elif num == 20:
+            advent20(data)
 
 if __name__ == "__main__":
     main()
